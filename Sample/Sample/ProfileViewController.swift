393a428142b1b8639239956fc8903e50a335775c
João Lucas <joaolucasfp2001@gmail.com>
2020-11-20 03:24:28 -0300

    Setup Sample

---
+++ Sample/Sample/ProfileViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ProfileViewController.swift
+//  Sample
+//
+//  Created by Joao Lucas on 20/11/20.
+//
+
+import UIKit
+
+class ProfileViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        self.view.backgroundColor = .red
+    }
+}
