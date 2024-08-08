e481bc79f19f3349aaef1af7574b85305e6b5300
joaolfp <joaolucasfp2001@gmail.com>
2024-08-07 22:58:49 -0300

    Adding improvements

--- Sources/ViewState/Observable.swift
+++ Sources/ViewState/Observable.swift
@@ -1,36 +1,37 @@
 import Foundation
 
 class Observable<T> {
-
+    
     typealias CompletionHandler = (T) -> Void
-
+    
     var value: T? {
         didSet {
             notifyObservers(observers)
         }
     }
-
+    
     var observers: [Int: CompletionHandler] = [:]
-
+    
     init(value: T?) {
         self.value = value
     }
-
+    
     func addObserver(_ observer: ObserverProtocol, completion: @escaping CompletionHandler) {
         observers[observer.id] = completion
+        if let value = value {
+            completion(value)
+        }
     }
-
+    
     func removeObserver(_ observer: ObserverProtocol) {
         observers.removeValue(forKey: observer.id)
     }
-
+    
     func notifyObservers(_ observers: [Int: CompletionHandler]) {
-        if value != nil {
-            guard let value = value else { return }
-            observers.forEach { $0.value(value) }
-        }
+        guard let value = value else { return }
+        observers.forEach { $0.value(value) }
     }
-
+    
     deinit {
         observers.removeAll()
     }
--- Sources/ViewState/ObserverProtocol.swift
+++ Sources/ViewState/ObserverProtocol.swift
@@ -1,14 +1,12 @@
 import Foundation
 
 protocol ObserverProtocol {
-
     var id: Int { get set }
     func onValueChanged(_ value: Any?)
 }
 
 protocol ObservableProtocol: AnyObject {
     var observers: [ObserverProtocol] { get set }
-
     func addObserver(_ observer: ObserverProtocol)
     func removeObserver(_ observer: ObserverProtocol)
     func notifyObservers(_ observers: [ObserverProtocol])
