3443970d2eec4a5310e14a0d53d7d898e3670a31
joaolfp <joaolucasfp2001@gmail.com>
2025-10-15 20:58:08 -0300

    Setup SwiftLint plugin

--- Package.swift
+++ Package.swift
@@ -3,6 +3,8 @@
 
 import PackageDescription
 
+let useSwiftLint = false
+
 let package = Package(
     name: "ViewState",
     platforms: [
@@ -18,10 +20,16 @@ let package = Package(
             targets: ["ViewState"]
         )
     ],
+    dependencies: useSwiftLint ? [
+        .package(url: "https://github.com/lukepistrol/SwiftLintPlugin", from: "0.61.0")
+    ] : [],
     targets: [
         .target(
             name: "ViewState",
-            dependencies: []
+            dependencies: [],
+            plugins: useSwiftLint ? [
+                .plugin(name: "SwiftLint", package: "SwiftLintPlugin")
+            ] : []
         ),
         .testTarget(
             name: "ViewStateTests",
